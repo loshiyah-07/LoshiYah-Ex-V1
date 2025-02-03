391943a38deb206a6c2c6318111a05355c5865b6
loshiyah-07 <spiderbloodstrike@gmail.com>
2025-02-04 04:38:49 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID, 
+ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/pgvev9.jpeg",
+ALIVE_MS: process.env.ALIVE_MSG || "Hellow I'm LoshiYah-Ex-V1 😎💥 I'm Alive Now 👋💗",
 };
